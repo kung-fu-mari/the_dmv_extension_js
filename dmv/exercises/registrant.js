03a6e315eea7b60e38ac3c57a9ed1689bd113fa3
kung-fu-mari <ifcodes@duck.com>
2024-12-17 17:16:54 -0700

    Initial commit

--- dmv/exercises/registrant.js
+++ dmv/exercises/registrant.js
@@ -1,7 +1,32 @@
+function Registrant(name, age, permit) {
+  this.name = name;
+  this.age = age;
+  this.permit = permit;
+  this.licenseData = {written: false, license: false, renewed: false};
+}
 
+function createRegistrant(name, age, permit = false) {
+  return new Registrant(name, age, permit)
+}
+
+function earnPermit(registrant) {
+  if (registrant.age >= 16) {
+    registrant.permit = true
+    return 'You have successfully earned a permit!'}
+  else {
+    return 'Sorry, you are not able to earn a permit right now.'}
+}
+
+function takeLicenseExam(registrant) {
+  if (registrant.permit == true) {
+    return 'Great job! You can take your test.'
+  }
+  else {
+    return 'Sorry, you need to earn a permit first.'}
+}
 
 module.exports = {
-  // createRegistrant,
-  // earnPermit,
-  // takeLicenseExam
+  createRegistrant,
+  earnPermit,
+  takeLicenseExam
 }
